51bd76fdef7198a6bb37b8d771decb0f37ecdc2c
Simon Peter Chappell <simonpeterchappell@gmail.com>
2010-07-29 11:53:26 -0500

    Converted all tests to JUnit 4.x

--- src/org/simonpeter/examples/fraction/Fraction.java
+++ src/org/simonpeter/examples/fraction/Fraction.java
@@ -3,141 +3,139 @@
  */
 
 package org.simonpeter.examples.fraction;
-import java.lang.IllegalArgumentException;
 
 /**
- * @author  Simon P. Chappell
- * @version $Id: Fraction.java 11 2008-07-18 21:21:13Z Simon $
+ * A classic mathematical fraction.
+ * 
+ * @author Simon Peter Chappell
+ * @version 20100728
  */
 public class Fraction {
 
-    final private int _numerator;
-    final private int _denominator;
+	final private int _numerator;
+	final private int _denominator;
 
-    public Fraction(int n, int d) {
-        if (d == 0) {
-            throw new IllegalArgumentException();
-        }
-        _numerator = n;
-        _denominator = d;
-    }
+	public Fraction(int n, int d) {
+		if (d == 0) { throw new IllegalArgumentException(); }
+		_numerator = n;
+		_denominator = d;
+	}
 
-    public int getNumerator() {
-        return _numerator;
-    }
+	public int getNumerator() {
+		return _numerator;
+	}
 
-    public int getDenominator() {
-        return _denominator;
-    }
+	public int getDenominator() {
+		return _denominator;
+	}
 
-    public boolean equals(Fraction f) {
-        boolean retVal = false;
-        
-        //
-        // Is the object not null?
-        //
-        if (f != null) {
-            //
-            // Are the objects of the same type?
-            //
-            if (f instanceof Fraction) {
-                //
-                // Are the instance variables identical?
-                //
-                if (_numerator == f.getNumerator() &&
-                    _denominator == f.getDenominator()) {
-                        retVal = true;
-                }
-            }
-        }
-        return (retVal);
-    }
+	/**
+	 * Indicates whether another {@link Fraction} is "equal to" this one.
+	 * <p>
+	 * A good description of the requirements for object equality is given in
+	 * the Java API document entry for {link Object}. An exhaustive explanation
+	 * of the qualities of a good {@code equals()} method is given in
+	 * <cite>Effective Java</cite> by Joshua Bloch.
+	 * 
+	 * @param obj - the reference object with which to compare.
+	 * @return {@code true} if this object is the same as the obj argument; {@code false} otherwise.
+	 * @see java.util.Object#equals
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) { return true; }
+		if (!(obj instanceof Fraction)) { return false; }
+		Fraction that = (Fraction) obj;
+		return (this._numerator   == that.getNumerator()
+		     && this._denominator == that.getDenominator());
+	}
 
-    public Fraction reduce() {
-        int u = _numerator;
-        int v = _denominator;
-        int temp;
-        
-        u = Math.abs(u);
-        
-        while (v != 0) {
-            temp = u % v;
-            u = v;
-            v = temp;
-        }
-        
-        return new Fraction(_numerator/u, _denominator/u);
-    }
+	/**
+	 * Returns a hash code value for the {@link Fraction}.
+	 * <p>
+	 * This method is supported for the benefit of hashtables, such as those
+	 * provided by {@link java.util.Hashtable}.
+	 * <p>
+	 * A good description of the requirements for object hashcodes is given in
+	 * the Java API document entry for {link Object}. An exhaustive explanation
+	 * of the qualities of a good {@code hashCode()} method is given in
+	 * <cite>Effective Java</cite> by Joshua Bloch.
+	 * 
+	 * @return a hash code value for this object.
+	 * @see java.util.Object#hashCode
+	 * @see java.util.Hashtable
+	 */
+	@Override
+	public int hashCode() {
+		int result = 17;
+		result = 37 * result + _numerator;
+		result = 37 * result + _denominator;
+		return result;
+	}
 
-    public boolean equivalent(Fraction f) {
-        boolean retVal = false;
-        
-        //
-        // Is the object not null?
-        //
-        if (f != null) {
-            //
-            // Are the objects of the same type?
-            //
-            if (f instanceof Fraction) {
-                //
-                // Are the reduced fractions identical?
-                //
-                Fraction f1 = this.reduce();
-                Fraction f2 = f.reduce();
-                if (f1.equals(f2)) {
-                    retVal = true;
-                }
-            }
-        }
-        return (retVal);
-    }
+	public Fraction reduce() {
+		int u = _numerator;
+		int v = _denominator;
+		int temp;
 
-    public Fraction add(Fraction f) {
-        // a/b + c/d = ((a*d) + (b*c)) / (b*d)
-        int a = _numerator;
-        int b = _denominator;
-        int c = f.getNumerator();
-        int d = f.getDenominator();
-        int num = ((a*d) + (b*c));
-        int den = (b*d);
+		u = Math.abs(u);
+		while (v != 0) {
+			temp = u % v;
+			u = v;
+			v = temp;
+		}
+		return new Fraction(_numerator / u, _denominator / u);
+	}
 
-        return new Fraction(num, den).reduce();
-    }
+	public boolean equivalent(Fraction that) {
+		return this.reduce().equals(that.reduce());
+	}
 
-    public Fraction sub(Fraction f) {
-        // a/b - c/d = ((a*d) - (b*c)) / (b*d)
-        int a = _numerator;
-        int b = _denominator;
-        int c = f.getNumerator();
-        int d = f.getDenominator();
-        int num = ((a*d) - (b*c));
-        int den = (b*d);
+	public Fraction add(Fraction f) {
+		// a/b + c/d = ((a*d) + (b*c)) / (b*d)
+		int a = _numerator;
+		int b = _denominator;
+		int c = f.getNumerator();
+		int d = f.getDenominator();
+		int num = ((a * d) + (b * c));
+		int den = (b * d);
 
-        return new Fraction(num, den).reduce();
-    }
+		return new Fraction(num, den).reduce();
+	}
 
-    public Fraction mul(Fraction f) {
-        // a/b * c/d = (a*c) / (b*d)
-        int a = _numerator;
-        int b = _denominator;
-        int c = f.getNumerator();
-        int d = f.getDenominator();
-        int num = a*c;
-        int den = b*d;
+	public Fraction sub(Fraction f) {
+		// a/b - c/d = ((a*d) - (b*c)) / (b*d)
+		int a = _numerator;
+		int b = _denominator;
+		int c = f.getNumerator();
+		int d = f.getDenominator();
+		int num = ((a * d) - (b * c));
+		int den = (b * d);
 
-        return new Fraction(num, den).reduce();
-    }
+		return new Fraction(num, den).reduce();
+	}
 
-    public Fraction div(Fraction f) {
-        // a/b / c/d = (a*d) / (b*c)
-        int a = _numerator;
-        int b = _denominator;
-        int c = f.getNumerator();
-        int d = f.getDenominator();
-        int num = a*d;
-        int den = b*c;
+	public Fraction mul(Fraction f) {
+		// a/b * c/d = (a*c) / (b*d)
+		int a = _numerator;
+		int b = _denominator;
+		int c = f.getNumerator();
+		int d = f.getDenominator();
+		int num = a * c;
+		int den = b * d;
 
-        return new Fraction(num, den).reduce();
-    }
+		return new Fraction(num, den).reduce();
+	}
+
+	public Fraction div(Fraction f) {
+		// a/b / c/d = (a*d) / (b*c)
+		int a = _numerator;
+		int b = _denominator;
+		int c = f.getNumerator();
+		int d = f.getDenominator();
+		int num = a * d;
+		int den = b * c;
+
+		return new Fraction(num, den).reduce();
+	}
 }
--- test/org/simonpeter/examples/fraction/FractionTest.java
+++ test/org/simonpeter/examples/fraction/FractionTest.java
@@ -3,175 +3,175 @@
  */
 
 package org.simonpeter.examples.fraction;
-import junit.framework.TestCase;
-import java.lang.IllegalArgumentException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 
 /**
- * @author  Simon P. Chappell
+ * @author Simon P. Chappell
  * @version $Id: FractionTest.java 11 2008-07-18 21:21:13Z Simon $
  */
-public class FractionTest extends TestCase {
+public class FractionTest {
 
-    public void testConstructor() {
-        //
-        // Known good value.
-        //
-        Fraction f1 = new Fraction(1, 2);
-        
-        //
-        // Known bad value.
-        //
-        try {
-            Fraction f2 = new Fraction(1, 0);
-            fail("Expected IllegalArgumentException");
-        } catch (IllegalArgumentException iae) {
-            assertTrue(true);
-        }
-    }
-    
-    
-    public void testNumeratorAccessor() {
-        //
-        // Known good values.
-        //
-        Fraction f1 = new Fraction(1, 2);
-        Fraction f2 = new Fraction(2, 3);
-        assertEquals(1, f1.getNumerator());
-        assertEquals(2, f2.getNumerator());
-    }
-    
-    
-    public void testDenominatorAccessor() {
-        //
-        // Known good values.
-        //
-        Fraction f1 = new Fraction(1, 2);
-        Fraction f2 = new Fraction(2, 3);
-        assertEquals(2, f1.getDenominator());
-        assertEquals(3, f2.getDenominator());
-    }
+	Fraction f1, f2, f3, f4;
+	Fraction oneFifth, twoFifths, threeFifths;
+	Fraction oneHalf, twoFourths, threeSixths;
 
-    public void testEquals() {
-        //
-        // Known good values.
-        //
-        Fraction f1 = new Fraction(1, 2);
-        Fraction f2 = new Fraction(1, 2);
-        assertTrue(f1.equals(f2));
-        
-        //
-        // Known bad values.
-        //
-        Fraction f3 = new Fraction(1, 3);
-        assertFalse(f1.equals(f3));
-        assertFalse(f1.equals(null));
-    }
+	@Before public void before() {
+		f1 = new Fraction(1,2);
+		f2 = new Fraction(1,2);
+		f3 = new Fraction(1,2);
+		f4 = new Fraction(3,7);
+		oneFifth = new Fraction(1,5);
+		twoFifths = new Fraction(2,5);
+		threeFifths = new Fraction(3,5);
+		oneHalf = new Fraction(1,2);
+		twoFourths = new Fraction(2,4);
+		threeSixths = new Fraction(3,6);
+	}
 
-    public void testReduce() {
-        Fraction f1 = new Fraction(1, 2);
-        Fraction f2 = new Fraction(2, 4);
-        Fraction f3 = f2.reduce();
-        assertTrue(f1.equals(f3));
+	@After public void after() {
+		f1 = null;
+		f2 = null;
+		f3 = null;
+		f4 = null;
+		oneFifth = null;
+		twoFifths = null;
+		threeFifths = null;
+		oneHalf = null;
+		twoFourths = null;
+		threeSixths = null;
+	}
 
-        Fraction f4 = new Fraction(3, 5);
-        Fraction f5 = new Fraction(9, 15);
-        Fraction f6 = f5.reduce();
-        assertTrue(f4.equals(f6));
-    }
-    
-    public void testEquivalent() {
-        Fraction f1 = new Fraction(1, 2);
-        Fraction f2 = new Fraction(2, 4);
-        assertTrue(f1.equivalent(f2));
+	@Test public void testConstructorWithGoodValues() {
+		new Fraction(1, 3);
+		new Fraction(3, 2);
+		new Fraction(1, 1);
+	}
 
-        Fraction f3 = new Fraction(1, 3);
-        Fraction f4 = new Fraction(2, 7);
-        assertFalse(f3.equivalent(f4));
+	@Test(expected=IllegalArgumentException.class)
+	public void testConstructorWithZeroDenominator() {
+		new Fraction(1, 0);
+	}
+
+	@Test public void testAccessors() {
+		assertEquals(1, f1.getNumerator());
+		assertEquals(2, f1.getDenominator());
+	}
+
+	@Test public void testReduce() {
+		assertEquals(oneHalf, twoFourths.reduce());
+		assertEquals(oneHalf, threeSixths.reduce());
+	}
+
+	@Test public void testEquivalent() {
+		assertTrue(oneHalf.equivalent(oneHalf));
+		assertTrue(oneHalf.equivalent(twoFourths));
+		assertFalse(oneHalf.equivalent(threeFifths));
+	}
+
+    /**
+     * Test that the {@code equals()} method handles nulls correctly.
+     * <p>
+     * An object is never equal to null.
+     */
+    @Test public void testNullBehaviorOfEquals() {
+    	assertFalse(f1.equals(null));
     }
 
-    public void testAddition() {
-        //
-        // Known good values.
-        //
-        Fraction f1 = new Fraction(1, 5);
-        Fraction f2 = new Fraction(2, 5);
-        Fraction f3 = new Fraction(3, 5);
-        Fraction f4 = f1.add(f2);
-        assertTrue(f4.equals(f3));
-
-        Fraction f5 = new Fraction(1, 3);
-        Fraction f6 = new Fraction(2, 5);
-        Fraction f7 = new Fraction(11, 15);
-        Fraction f8 = f5.add(f6);
-        assertTrue(f8.equals(f7));
-
-        //
-        // Known bad values.
-        //
-        f1 = new Fraction(2, 5);
-        f2 = new Fraction(2, 5);
-        f3 = new Fraction(3, 5);
-        f4 = f1.add(f2);
-        assertFalse(f4.equals(f3));
+    /**
+     * Test that the {@code equals()} method is reflexive.
+     * <p>
+     * An object is always equal to itself.
+     */
+    @Test public void testReflexiveBehaviorOfEquals() {
+    	assertEquals(f1, f1);
+    	assertTrue(f1.equals(f1));
     }
 
-    public void testSubtraction() {
-        //
-        // Known good values.
-        //
-        Fraction f1 = new Fraction(3, 5);
-        Fraction f2 = new Fraction(2, 5);
-        Fraction f3 = new Fraction(1, 5);
-        Fraction f4 = f1.sub(f2);
-        assertTrue(f4.equals(f3));
-
-        Fraction f5 = new Fraction(2, 3);
-        Fraction f6 = new Fraction(1, 5);
-        Fraction f7 = new Fraction(7, 15);
-        Fraction f8 = f5.sub(f6);
-        assertTrue(f8.equals(f7));
-
-        //
-        // Known bad values.
-        //
-        f1 = new Fraction(3, 5);
-        f2 = new Fraction(1, 5);
-        f3 = new Fraction(3, 5);
-        f4 = f1.add(f2);
-        assertFalse(f4.equals(f3));   
-    }
-    
-    public void testMultiplication() {
-        //
-        // Known good values.
-        //
-        Fraction f1 = new Fraction(3, 5);
-        Fraction f2 = new Fraction(2, 5);
-        Fraction f3 = new Fraction(6, 25); // expected answer
-        Fraction f4 = f1.mul(f2);
-        assertTrue(f4.equals(f3));
-
-        Fraction f5 = new Fraction(1, 2);
-        Fraction f6 = new Fraction(1, 2);
-        Fraction f7 = new Fraction(1, 4); // expected answer
-        Fraction f8 = f5.mul(f6);
-        assertTrue(f8.equals(f7));
+    /**
+     * Test that the {@code equals()} method handles unequal values correctly.
+     * <p>
+     * If o1 does not equal o2, then o2 must not equal o1.
+     */
+    @Test public void testUnequalValuesOfEqual() {
+    	assertFalse(f1.equals(f4));
+    	assertFalse(f4.equals(f1));
     }
 
-    public void testDivision() {
-        //
-        // Known good values.
-        //
-        Fraction f1 = new Fraction(3, 5);
-        Fraction f2 = new Fraction(2, 5);
-        Fraction f3 = new Fraction(3, 2); // expected answer
-        Fraction f4 = f1.div(f2);
-        assertTrue(f4.equals(f3));
-
-        Fraction f5 = new Fraction(1, 2);
-        Fraction f6 = new Fraction(1, 2);
-        Fraction f7 = new Fraction(1, 1); // expected answer
-        Fraction f8 = f5.div(f6);
-        assertTrue(f8.equals(f7));
+    /**
+     * Test that the {@code equals()} method is symmetrical.
+     * <p>
+     * If o1 is equal to o2, then o2 must be equal to o1.
+     */
+    @Test public void testSymmetricalBehaviorOfEquals() {
+    	assertTrue(f1.equals(f2));
+    	assertTrue(f2.equals(f1));
     }
+
+    /**
+     * Test that the {@code equals()} method is transitive.
+     * <p>
+     * If o1 is equal to o2 and o2 is equal to o3, then o3 must be equal to o1.
+     */
+    @Test public void testTransitiveBehaviorOfEquals() {
+    	assertTrue(f1.equals(f2));
+    	assertTrue(f2.equals(f3));
+    	assertTrue(f3.equals(f1));
+    }
+
+    /**
+     * Test that hashcodes are equal for equal objects.
+     * <p>
+     * Objects that are equal, must have equal hashcodes.
+     * Interestingly, there is no requirement for unequal
+     * objects to have different hashcodes.
+     * <p>
+     * A classic cheat for hashcodes is to always return
+     * an integer literal, such as 1. This is legal, but
+     * causes very inefficient hashing behavior in most
+     * collection objects.
+     */
+    @Test public void testEqualObjectsHaveEqualHashCodes() {
+    	assertEquals(f1.hashCode(), f2.hashCode());
+    }
+
+    /**
+     * Test that an object has a constant hashcode.
+     * <p>
+     * An unchanged object should always have the same hashcode within
+     * any lifetime within an instance of the JVM.
+     * <p>
+     * The default behavior for the {@code hashCode()} method is to return
+     * the memory address of the object instance. Obviously this can change
+     * between runs of a program, but would be a constant through the
+     * duration of each run.
+     */
+    @Test public void testSameObjectAlwaysSameHashCode() {
+    	int h1 = f1.hashCode();
+    	int h2 = f1.hashCode();
+    	assertEquals(h1, h2);
+    }
+
+	@Test public void testAddition() {
+		assertEquals(threeFifths, oneFifth.add(twoFifths));
+	}
+
+	@Test public void testSubtraction() {
+		assertEquals(twoFifths, threeFifths.sub(oneFifth));
+	}
+
+	@Test public void testMultiplication() {
+		assertEquals(new Fraction(6,25), threeFifths.mul(twoFifths));
+	}
+
+	@Test public void testDivision() {
+		assertEquals(new Fraction(3,2), threeFifths.div(twoFifths));
+		assertEquals(new Fraction(1,1), oneHalf.div(oneHalf));
+	}
 }
